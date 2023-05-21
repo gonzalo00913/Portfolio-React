76b129da1d5a8acc080d50d468efcc2d4137bca9
gonzalo00913 <gonzalo-masa@outlook.com>
2023-05-20 21:06:22 -0300

    cambios nav y inicio margen

--- src/componentes/Nav/Nav.jsx
+++ src/componentes/Nav/Nav.jsx
@@ -11,18 +11,22 @@ const Nav = () => {
         </div>
         <div className="container-links ">
           <NavLink className="link home" to="/" exact title="Home">
-            <i className="fa-solid fa-house"></i>
+            <i className="fa-solid fa-house"></i><br />
+            <span className="nombre-icono">Home</span>
           </NavLink>
 
           <NavLink className="link" to="/habilidades" title="Skills">
-            <i className="fa-solid fa-gear"></i>
+            <i className="fa-solid fa-gear"></i><br />
+            <span className="nombre-icono">Skills</span>
           </NavLink>
 
           <NavLink className="link" to="/proyectos" title="Projects">
-            <i className="fa-solid fa-sheet-plastic"></i>
+            <i className="fa-solid fa-sheet-plastic"></i><br />
+            <span className="nombre-icono">Projects</span>
           </NavLink>
           <NavLink className="link contacto" to="/contact" title="Contact">
-            <i className="fa-solid fa-phone"></i>
+            <i className="fa-solid fa-phone"></i><br />
+            <span className="nombre-icono">Contact</span>
           </NavLink>
         </div>
       </nav>
